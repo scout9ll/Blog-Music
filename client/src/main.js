c284437b47269acc38bae85b7294e69415754db7
steve9ll <qiuliaolin1@163.com>
2019-10-04 19:20:11 +0800

    fix:change song

--- client/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import App from "./App.vue";
-import myHandDrawToast from "./plugins/myHandDrawToast";
-Vue.config.productionTip = false;
-
-Vue.use(myHandDrawToast);
-new Vue({
-  render: h => h(App)
-}).$mount("#app");
